772748e64c284f381bd635dba8da80c9873b24b1
Iwaide <iwaide@hoobby.net>
2020-01-13 16:25:13 +0900

    Add email filed

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -13,9 +13,30 @@ class LoginScreenState extends State<LoginScreen> {
       margin: EdgeInsets.all(20.0),
       child: Form(
         child: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            emailField(),
+            // passwordField(),
+            // submitButton()
+          ],
         ),
       ),
     );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com'
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    
+  }
+
+  Widget submitButton() {
+
   }
 }
